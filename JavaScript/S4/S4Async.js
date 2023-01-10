620f7d06c75df6db38764fc62911d68d28b292cd
Anna-1902 <annaipsen19@gmail.com>
2023-01-10 14:02:11 +0100

    code

--- JavaScript/S4/S4Async.js
+++ JavaScript/S4/S4Async.js
@@ -8,15 +8,31 @@ const multi = (a, b) => {
 const divide = (a, b) => {
     return a / b
 }
-const subTwo = (a,b) => {
+const subTwo = (a, b) => {
     return a - b
 }
 
-function calc (a, b, callback) {
+function calc(a, b, callback) {
     return callback(a, b)
 }
 
-console.log(calc(4,5, addTwo))
-console.log(calc(5,5, multi))
-console.log(calc(6,3, divide))
-console.log(calc(6,5, subTwo))
+console.log(calc(4, 5, addTwo))
+console.log(calc(5, 5, multi))
+console.log(calc(6, 3, divide))
+console.log(calc(6, 5, subTwo))
+
+// promise
+//Book array
+let bookArray = [];
+
+//Load books from JSON
+function showBook() {
+    fetch('./books.json')
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+}
+
+
+showBook();
+
